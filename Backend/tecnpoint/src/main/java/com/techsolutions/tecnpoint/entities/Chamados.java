e9c6fa8bba2d4017c4fb2333bf834ad9016250bc
samuel <samuelconnde@gmail.com>
2025-10-03 18:56:01 -0300

    Ajustando imports da classe Chamados

--- Backend/tecnpoint/src/main/java/com/techsolutions/tecnpoint/entities/Chamados.java
+++ Backend/tecnpoint/src/main/java/com/techsolutions/tecnpoint/entities/Chamados.java
@@ -3,12 +3,12 @@ package com.techsolutions.tecnpoint.entities;
 import com.techsolutions.tecnpoint.enums.PrioridadeChamado;
 import com.techsolutions.tecnpoint.enums.StatusChamado;
 import jakarta.persistence.*;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Entity
 @Table(name = "chamados")
