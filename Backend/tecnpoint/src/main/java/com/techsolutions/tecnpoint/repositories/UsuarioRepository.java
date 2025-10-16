0b70de613383036114b2817827f81b85ae8f665e
samuel <samuelconnde@gmail.com>
2025-10-15 21:24:57 -0300

    Adicionando listagem de nome dos funcionários.

--- Backend/tecnpoint/src/main/java/com/techsolutions/tecnpoint/repositories/UsuarioRepository.java
+++ Backend/tecnpoint/src/main/java/com/techsolutions/tecnpoint/repositories/UsuarioRepository.java
@@ -1,11 +1,13 @@
 package com.techsolutions.tecnpoint.repositories;
 
-
 import com.techsolutions.tecnpoint.entities.Usuarios;
+import com.techsolutions.tecnpoint.enums.TipoUsuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuarios, Long>{
     public boolean existsByEmail(String email);
+    public List<Usuarios> findByTipoUsuario(TipoUsuario tipoUsuario);
 }
