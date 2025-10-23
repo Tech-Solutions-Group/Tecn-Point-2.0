131a44cbbddff591d5af8437cd43bf0b71e70ba9
samuel <samuelconnde@gmail.com>
2025-10-23 16:53:33 -0300

    Adicionando Id da conversa no DTO VisualizacaoConversaDTO

--- Backend/tecnpoint/src/main/java/com/techsolutions/tecnpoint/DTO/VisualizacaoConversaDTO.java
+++ Backend/tecnpoint/src/main/java/com/techsolutions/tecnpoint/DTO/VisualizacaoConversaDTO.java
@@ -1,14 +1,19 @@
 package com.techsolutions.tecnpoint.DTO;
 
 import com.techsolutions.tecnpoint.entities.Usuarios;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class VisualizacaoConversaDTO {
+    Long idConversa;
     VisualizacaoUsuarioDTO remetente;
     String mensagem;
     LocalDateTime dataHoraEnvio;
