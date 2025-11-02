a81fc46fccb31fdbdc47ee800d5ecf0d899ebf74
Pedro Vieira <pedronicolas576@gmail.com>
2025-11-02 20:51:59 -0300

    Adicionando service e componente para pagina de chamado e coversas

--- Frontend/src/app/service/chamado.service.ts
+++ Frontend/src/app/service/chamado.service.ts
@@ -46,8 +46,4 @@ export class ChamadoService {
   patchChamado(id: number, data: Partial<Chamado>): Observable<Chamado> {
     return this.http.patch<Chamado>(`${this.apiUrl}/${id}`, data);
   }
-
-  delChamado(id: number): Observable<void> {
-    return this.http.delete<void>(`${this.apiUrl}/${id}`);
-  }
 }
